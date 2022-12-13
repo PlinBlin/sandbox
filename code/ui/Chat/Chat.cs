0a212fa8af75f88568ce086dcc2fe4f49940baf0
Tony Ferguson <devultj@gmail.com>
2022-12-13 17:27:05 +0000

    API update, minor cleanup

--- code/ui/Chat/Chat.cs
+++ code/ui/Chat/Chat.cs
@@ -2,13 +2,13 @@ namespace Sandbox;
 
 public partial class Chat
 {
-	[ConCmd.Client( "sandbox_chat_add", CanBeCalledFromServer = true )]
+	[ConCmd.Client( "chat_add", CanBeCalledFromServer = true )]
 	public static void AddChatEntry( string name, string message, string playerId = "0", bool isInfo = false )
 	{
 		Current?.AddEntry( name, message, long.Parse( playerId ), isInfo );
 
 		// Only log clientside if we're not the listen server host
-		if ( !Global.IsListenServer )
+		if ( !Game.IsListenServer )
 		{
 			Log.Info( $"{name}: {message}" ); 
 		}
@@ -16,25 +16,20 @@ public partial class Chat
 
 	public static void AddChatEntry( To target, string name, string message, long playerId = 0, bool isInfo = false )
 	{
+		// Can't use long on ConCmd :<
 		AddChatEntry( target, name, message, playerId.ToString(), isInfo );
 	}
 
-	[ConCmd.Client( "sandbox_addinfo", CanBeCalledFromServer = true )]
-	public static void AddInformation( string message )
-	{
-		Current?.AddEntry( null, message );
-	}
-
-	[ConCmd.Server( "sandbox_say" )]
+	[ConCmd.Server( "say" )]
 	public static void Say( string message )
 	{
-		Assert.NotNull( ConsoleSystem.Caller );
+		if ( !ConsoleSystem.Caller.IsValid() ) return;
 
 		// todo - reject more stuff
 		if ( message.Contains( '\n' ) || message.Contains( '\r' ) )
 			return;
 
 		Log.Info( $"{ConsoleSystem.Caller}: {message}" );
-		AddChatEntry( To.Everyone, ConsoleSystem.Caller.Name, message, ConsoleSystem.Caller.PlayerId );
+		AddChatEntry( To.Everyone, ConsoleSystem.Caller.Name, message, ConsoleSystem.Caller.SteamId );
 	}
 }
